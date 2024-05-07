5bece2fa7071d9140cbbd0abb72a0fa36d9792d9
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-07 11:13:59 -0400

    AULA 2 - SUBINDO O SERVIDOR

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
+// AULA 2 - Subindo o servidor
+
 package main
 
-func main() {
+import (
+	"net/http"
+	"text/template"
+)
 
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil) // Liberar porta no servidor local
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Index", nil)
 }
