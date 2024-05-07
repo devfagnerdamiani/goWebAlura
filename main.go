83b9e9b7f626190ceb3ba45ff6abdf5dee2f3fc7
Fagner Damiani <fagnerdamiani@gmail.com>
2024-05-07 11:39:48 -0400

    AULA 3 - CRIANDO STRUCT DE PRODUTOS

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// AULA 2 - Subindo o servidor
+// AULA 3 - Criando struct de produtos
 
 package main
 
@@ -6,6 +6,13 @@ import (
 	"net/http"
 	"text/template"
 )
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -15,5 +22,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+
+	produtos := []Produto{{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
+		{"Tenis", "Confortável", 89, 3}, {"Fone", "Muito bom", 59, 2},
+		{"Produto Novo", "Muito legal", 1.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", produtos)
+
 }
